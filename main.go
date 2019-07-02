616cec8384911fcd2899e6b1c9037f58fd2883b6
lukluk.santoso <lukluk.santoso@go-pay.co.id>
2019-07-02 15:41:48 +0700

    build new

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"os"
 
 	"github.com/lukluk/tcpproxy"
@@ -10,10 +11,10 @@ import (
 func main() {
 	if len(os.Args) < 1 {
 		fmt.Println("params [LOCAL PORT] [TARGET_IP:PORT]")
-		return
 	}
 	localPort := os.Args[1]
 	targetHost := os.Args[2]
-	proxy := tcpproxy.Proxy{}
+	var proxy tcpproxy.Proxy
 	proxy.AddRoute(":"+localPort, tcpproxy.To(targetHost))
+	log.Fatal(proxy.Run())
 }
